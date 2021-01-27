d557061425522e9369c3af8c8b8150e08aa9f040
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-27 06:10:59 -0300

    added #getPaginated tests

--- test/pagination.test.js
+++ test/pagination.test.js
@@ -86,5 +86,28 @@ describe('Pagination tests', () => {
         assert.deepStrictEqual(result, data)
       });
     });
+
+    describe('#getPaginated', () => {
+      const responseMock = [
+        {
+          "tid": 5705,
+          "date": 1373123005,
+          "type": "sell",
+          "price": 196.52,
+          "amount": 0.01,
+        },
+        {
+          "tid": 5706,
+          "date": 1373124523,
+          "type": "buy",
+          "price": 200,
+          "amount": 0.3,
+        }
+      ];
+
+      it('should update request id on each requests');
+
+      it('should stop requesting when request return an empty array');
+    });
   });
 });
