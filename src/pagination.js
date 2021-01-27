514a20e13e22fe0e93c6c74e63d1daf094c7c528
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-27 05:41:08 -0300

    ensure retry a request twice before throwing an exception

--- src/pagination.js
+++ src/pagination.js
@@ -17,7 +17,32 @@ class Pagination {
     this.threshold = options.threshold;
   }
 
-  async handleRequest({ url, page, retries = 1 }) {}
+  async handleRequest({ url, page, retries = 1 }) {
+    try {
+      const finalUrl = `${url}?tid=${page}`;
+      const result = await this.request.makeRequest({
+        url: finalUrl,
+        method: 'get',
+        timeout: this.maxRequestTimeout,
+      });
+
+      return result;
+    } catch (error) {
+      if (retries === this.maxRetries) {
+        console.error(`[${retries}] max retries reached!`);
+        throw error;
+      }
+
+      console.error(`[${retries}] an error: [${error.message}] has happened! Trying again in ${this.retryTimeout}ms`);
+      await Pagination.sleep(this.retryTimeout);
+
+      return this.handleRequest({ url, page, retries: retries += 1 });
+    }
+  }
+
+  static async sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
   async getPaginated({ url, page }) {}
 }
--- test/pagination.test.js
+++ test/pagination.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 
 const { describe, it, before, afterEach } = require('mocha');
 const { createSandbox } = require('sinon');
+const Pagination = require('../src/pagination');
 
 describe('Pagination tests', () => {
   let sandbox;
@@ -13,7 +14,48 @@ describe('Pagination tests', () => {
   afterEach(() => sandbox.restore());
 
   describe('#Pagination', () => {
-    it('should retry an request twice before throing an exception and validate request params and flow');
+    it('should retry a request twice before throwing an exception and validate request params and flow', async () => {
+      const expectedCallCount = 2;
+      const expectedTimeout = 2;
+      
+      const pagination = new Pagination();
+      pagination.maxRetries = expectedCallCount;
+      pagination.retryTimeout = expectedTimeout;
+      pagination.maxRequestTimeout = expectedTimeout;
+
+      const error = new Error("timeout");
+      
+      // mostra quantas vezes o método é rodado
+      sandbox.spy(pagination, pagination.handleRequest.name);
+      sandbox.stub(
+        Pagination,
+        Pagination.sleep.name,
+      ).resolves();
+
+      sandbox.stub(
+        pagination.request,
+        pagination.request.makeRequest.name,
+      ).rejects(error);
+
+      const dataRequest = { url: 'https://google.com', page: 0 };
+      await assert.rejects(pagination.handleRequest(dataRequest), error);
+      assert.deepStrictEqual(pagination.handleRequest.callCount, expectedCallCount);
+
+      const lastCall = expectedCallCount - 1;
+      const lastCallArg = pagination.handleRequest.getCall(lastCall).lastArg;
+      const lastCallRetries = lastCallArg.retries;
+      assert.deepStrictEqual(lastCallRetries, expectedCallCount);
+
+      const expectedArgs = {
+        url: `${dataRequest.url}?tid=${dataRequest.page}`,
+        method: 'get',
+        timeout: expectedTimeout,
+      };
+      const firstCallArgs = pagination.request.makeRequest.getCall(0).args;
+      assert.deepStrictEqual(firstCallArgs, [expectedArgs]);
+
+      assert.ok(Pagination.sleep.calledWithExactly(expectedTimeout));
+    });
 
     it('should return data from request when succeded');
   });
