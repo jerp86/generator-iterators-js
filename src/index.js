bdaf6f1d9c8d87f035eb1229de252afb03e272c9
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-27 07:18:56 -0300

    updated index

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
-const Request = require('./request');
-const request = new Request();
+const Pagination = require('./pagination');
 
-async function scheduler() {
-  console.log('\nstarting in...', new Date().toISOString());
-  const requests = [
-    { url: 'https://www.mercadobitcoin.net/api/BTC/ticker/' },
-    { url: 'https://www.NAO_EXISTE.net' },
-    { url: 'https://www.mercadobitcoin.net/api/BTC/orderbook/' },
-  ]
-    .map(data => ({
-      ...data,
-      timeout: 2000,
-      method: 'get',
-    }))
-    .map(params => request.makeRequest(params));
+;(async () => {
+  const pagination = new Pagination();
 
-  const result = await Promise.allSettled(requests);
-  const allSucceded = [];
-  const allFailed = [];
+  const firstPage = 770e3;
+  const req = pagination.getPaginated({
+    url: 'https://www.mercadobitcoin.net/api/BTC/trades/',
+    page: firstPage,
+  });
 
-  for (const { status, value, reason } of result) {
-    // if (status === 'rejected') {
-    //   allFailed.push(reason);
-    //   continue;
-    // }
-
-    // allSucceded.push(value);
-    status === 'rejected' ? allFailed.push(reason) : allSucceded.push(value);
+  for await (const items of req) {
+    console.table(items);
   }
-
-  console.log({
-    allFailed,
-    allSucceded: JSON.stringify(allSucceded)
-  })
-}
-
-const PERIOD = 2000;
-setInterval(scheduler, PERIOD);
+})();
